901b730bbc6848cfee6113b35eb75fa4b52b1827
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2025-01-26 22:31:45 +0100

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { motion } from "framer-motion";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import useLenis from "./lenis";
+import ScrollToTop from "./scroll";
 
 const App = () => {
-  useLenis()
+  useLenis();
+
+  const MotionWrapper = ({ children }: { children: React.ReactNode }) => (
+    <motion.div
+      initial={{ opacity: 0}}
+      animate={{ opacity: 1}}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.3 }}
+    >
+      {children}
+    </motion.div>
+  );
+
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<MotionWrapper><Home /></MotionWrapper>} />
+        <Route path="/about" element={<MotionWrapper><About /></MotionWrapper>} />
+        <Route path="/services" element={<MotionWrapper><Services /></MotionWrapper>} />
+        <Route path="/contact" element={<MotionWrapper><Contact /></MotionWrapper>} />
+        <Route path="*" element={<MotionWrapper><NotFound /></MotionWrapper>} />
       </Routes>
     </Router>
   );
